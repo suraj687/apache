1c82841c17d98ec38f89ed3ee10a607a9da8a151
Surya <63136026+suraj687@users.noreply.github.com>
2020-07-22 00:17:27 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,7 +3,7 @@
 sudo echo "how to install httpd with shell script"
 sudo yum install httpd -y
 sudo systemctl start httpd
-sudo echo "hii this is test hii this is the my fierst job" > /var/www/html/index.html
+sudo echo "hii this is test, this is the my first job to webhook" > /var/www/html/index.html
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --reload
 
